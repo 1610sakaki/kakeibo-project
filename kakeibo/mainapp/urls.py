2e56362605ac7db32bb8864337b17a6d85ce8887
HirotoSakaki <skkcode1610@gmail.com>
2022-05-23 14:41:28 +0900

    トップページから収入・支出の登録をできるようにした

--- kakeibo/mainapp/urls.py
+++ kakeibo/mainapp/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('kakeibo/income', views.ListIncomeView.as_view()),
+    path('kakeibo/', views.ListIncomeView.as_view(), name='kakeibo-main'),
+    path('kakeibo/income/list', views.ListIncomeView.as_view(), name='list-income'),
+    path('kakeibo/payment/list',
+         views.ListPaymentView.as_view(), name='list-payment'),
+    path('kakeibo/income/create',
+         views.CreateIncomeView.as_view(), name='create-income'),
+    path('kakeibo/payment/create',
+         views.CreatePaymentView.as_view(), name='create-payment'),
 ]
--- kakeibo/mainapp/views.py
+++ kakeibo/mainapp/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-from .models import Income
+from django.urls import reverse_lazy
+
+from .models import Income, Payment
 
 
 class ListIncomeView(ListView):
     template_name = 'mainapp/income_list.html'
     model = Income
+    '''
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context)
+        return context
+    '''
+
+
+class ListPaymentView(ListView):
+    template_name = 'mainapp/payment_list.html'
+    model = Payment
+
+
+class CreateIncomeView(CreateView):
+    template_name = 'mainapp/income_create.html'
+    model = Income
+    fields = ('date', 'category', 'price', 'description')
+    success_url = reverse_lazy('list-income')
+
+
+class CreatePaymentView(CreateView):
+    template_name = 'mainapp/payment_create.html'
+    model = Payment
+    fields = ('date', 'category', 'price', 'description')
+    success_url = reverse_lazy('list-payment')
