30b860d6c4e8a017982bc433817b8f250a4999f8
HirotoSakaki <skkcode1610@gmail.com>
2022-05-23 16:20:24 +0900

    update delete機能追加

--- kakeibo/mainapp/urls.py
+++ kakeibo/mainapp/urls.py
@@ -3,15 +3,25 @@ from . import views
 
 urlpatterns = [
     path('kakeibo/', views.ListIncomeView.as_view(), name='kakeibo-main'),
+    # income
     path('kakeibo/income/list', views.ListIncomeView.as_view(), name='list-income'),
     path('kakeibo/income/<int:pk>/detail',
          views.DetailIncomeView.as_view(), name='detail-income'),
     path('kakeibo/income/create',
          views.CreateIncomeView.as_view(), name='create-income'),
+    path('kakeibo/income/<int:pk>/update',
+         views.UpdateIncomeView.as_view(), name='update-income'),
+    path('kakeibo/income/<int:pk>/delete',
+         views.DeleteIncomeView.as_view(), name='delete-income'),
+    # payment
     path('kakeibo/payment/list',
          views.ListPaymentView.as_view(), name='list-payment'),
     path('kakeibo/payment/<int:pk>/detail',
          views.DetailPaymentView.as_view(), name='detail-payment'),
     path('kakeibo/payment/create',
          views.CreatePaymentView.as_view(), name='create-payment'),
+    path('kakeibo/payment/<int:pk>/update',
+         views.UpdatePaymentView.as_view(), name='update-payment'),
+    path('kakeibo/payment/<int:pk>/delete',
+         views.DeletePaymentView.as_view(), name='delete-payment'),
 ]
--- kakeibo/mainapp/views.py
+++ kakeibo/mainapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import(
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 
 from django.urls import reverse_lazy
 
@@ -45,3 +51,31 @@ class DetailIncomeView(DetailView):
 class DetailPaymentView(DetailView):
     template_name = 'mainapp/payment_detail.html'
     model = Payment
+
+
+class UpdateIncomeView(UpdateView):
+    template_name = 'mainapp/income_update.html'
+    model = Income
+    fields = ('date', 'category', 'price', 'description')
+    success_url = reverse_lazy('list-income')
+
+
+class UpdatePaymentView(UpdateView):
+    template_name = 'mainapp/payment_update.html'
+    model = Payment
+    fields = ('date', 'category', 'price', 'description')
+    success_url = reverse_lazy('list-payment')
+
+
+class DeleteIncomeView(DeleteView):
+    template_name = 'mainapp/income_delete.html'
+    model = Income
+    fields = ('date', 'category', 'price', 'description')
+    success_url = reverse_lazy('list-income')
+
+
+class DeletePaymentView(DeleteView):
+    template_name = 'mainapp/payment_delete.html'
+    model = Payment
+    fields = ('date', 'category', 'price', 'description')
+    success_url = reverse_lazy('list-payment')
